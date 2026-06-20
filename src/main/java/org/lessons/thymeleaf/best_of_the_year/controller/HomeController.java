f0a97520d33f45c3afb46a826e191f0a87eeb1a3
SimoneZoroddu <lenviur@gmail.com>
2026-06-20 09:46:09 +0200

    first commit

---
+++ src/main/java/org/lessons/thymeleaf/best_of_the_year/controller/HomeController.java
@@ -0,0 +1,19 @@
+package org.lessons.thymeleaf.best_of_the_year.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    public String name = "Simone";
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("name", name);
+        return "home";
+    }
+}
